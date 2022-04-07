1622b777952e022839a7e1fb1b31e17781acfb08
jang walia <jangiwalia83@gmail.com>
2022-04-07 10:57:46 -0700

    seed employees table

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,11 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Jang", last_name: "walia", hourly_rate: 50)
+@store1.employees.create(first_name: "Tom", last_name: "Smith", hourly_rate: 45)
+
+@store2.employees.create(first_name: "Ted", last_name: "Baker", hourly_rate: 50)
+@store2.employees.create(first_name: "John", last_name: "ford", hourly_rate: 55)
+@store2.employees.create(first_name: "James", last_name: "parker", hourly_rate: 60)
