769ad8cff472605bd9bfa8e1e959cc49c0c10ea7
jang walia <jangiwalia83@gmail.com>
2022-04-07 10:41:40 -0700

    show stores average and total revenue

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,12 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+# total revenue
+@total_revenue = Store.sum(:annual_revenue)
+puts @total_revenue
+#average annual revenue
+@average_revenue = Store.average(:annual_revenue)
+puts @average_revenue
+#stores with revenue over 1million
+@revenue_over_million = Store.where("annual_revenue >= ?", 1000000).count
+puts @revenue_over_million
