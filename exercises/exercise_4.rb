2a9f9a1c188c8abdbb6b3255f2a3c4b39845293a
jang walia <jangiwalia83@gmail.com>
2022-04-07 10:26:26 -0700

    fetch data using where class

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,17 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+# adding three more stores
+store5 = Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+store6 = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+store7 = Store.create(name: "Yaletown", annual_revenue: 4300000, mens_apparel: true, womens_apparel: false)
+
+#fetching only those stores which sell men's apparell
+
+@mens_stores = Store.where(mens_apparel: true)
+@mens_stores.each do |store|
+  puts store.name
+  puts store.annual_revenue
+end
+
+@womens_stores = Store.where("womens_apparel = true AND annual_revenue < ?",1000000)
